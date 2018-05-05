0f9e10f1186e8e316e8327212f005682f2200312
VictorLima1 <victormanoel119@gmail.com>
2018-05-05 13:12:07 -0300

    Comentando todo o código

--- Tentativa 1/src/sample/Main.java
+++ Tentativa 1/src/sample/Main.java
@@ -10,12 +10,16 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
+    // Isso aqui é o começo, nunca vai mudar, então não se preocupem
     @Override
     public void start(Stage primaryStage) throws Exception{
+        // Essa linha define o que vai ser carregado (qual arquivo no caso)
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        // Essa linha aqui tira a borda dele, por isso que não aparecem os controles padrão da janela (minimizar, maximixar e fechar)
         primaryStage.initStyle(StageStyle.TRANSPARENT);
+        // As linhas abaixo definem a tela e fazem ela aparecer
         Scene scene = new Scene(root);
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("RPG");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
