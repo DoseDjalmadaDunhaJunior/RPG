8f5cf752cdb163bedb300b68d5b5718a9182b8f0
yoru <juanitplays@gmail.com>
2018-05-25 16:34:30 -0300

    add morte

--- src/sample/ControllerAtaque.java
+++ src/sample/ControllerAtaque.java
@@ -24,7 +24,7 @@ public class ControllerAtaque implements Initializable{
     public void initialize(URL url, ResourceBundle rb) {
         List<String> dados = new ArrayList<String>(jogadores-1);
         for(int i = 0; i < (jogadores); i ++){
-            dados.add(fichas.get(i).getNome());
+            if(fichas.get(i).getMorto() == false)dados.add(fichas.get(i).getNome());
             System.out.println(fichas.get(i).getNome());
         }
 
@@ -60,6 +60,7 @@ public class ControllerAtaque implements Initializable{
         for(int i = 0; i < jogadores; i++){
             if(fichas.get(i).getNome() == jogador){
                 fichas.get(i).setMana((fichas.get(i).getMana() - dano));
+                if(fichas.get(i).getMana()<=0) fichas.get(i).setMorto(true);
             }
         }
 
--- src/sample/Ficha.java
+++ src/sample/Ficha.java
@@ -45,6 +45,14 @@ public class Ficha {
         this.XP = XP;
     }
 
+    public boolean getMorto() {
+        return Morto;
+    }
+
+    public void setMorto(boolean morto) {
+        Morto = morto;
+    }
+
     public void setInt(int Int) {
         this.Int = Int;
     }
@@ -58,5 +66,6 @@ public class Ficha {
     private int mana;
     private int XP;
     private int Int;
+    private boolean Morto = false;
 
 }
